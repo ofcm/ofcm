b11ebbffdc93fcbba3023e7a561e471412b55b64
erickTorneroT <erickdeivy01@gmail.com>
2019-02-21 09:00:06 -0500

    base of machine state

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,52 @@
 #include "headers/miscellanius.hpp"
 #include "headers/SVMHandler.hpp"
 
-int main(int argc, char** argv){   
-    std::vector<std::vector<float>> centers;
+// Mode
+// 0: Train K-Means Centers
+// 1: Generate data of training
+// 2: Training SVM classifier
+// 3: Prediction mode
 
+int mode = 0;
+int main(int argc, char** argv){   
     int K_CLASSES = 2*6;
-    
+    std::vector<std::vector<float>> centers;
+    switch (mode)
+    {
+        case 0: {
+            int res1 = getCentroids(centers, K_CLASSES);
+            break;
+        }
+        case 1: {
+            std::string modelcenters_file = "../models/centroids/centers_model.txt";
+            SingleFileHandler <float> fhandler(modelcenters_file);
+            fhandler.LoadFromFile(centers);
+            int res2 = getHistograms(centers, K_CLASSES);
+            break;
+        }
+        case 2: {
+            std::string trainingfile = "../models/training/trainingdata.txt";
+            std::string labelsfile   = "../models/training/labeldata.txt";
+            FileHandlerML <int> fhandler(trainingfile, labelsfile);
+
+            std::vector<std::vector<int>> data;
+            std::vector<int> y;
+            std::vector<int> lbls {0, 1, 2, 3, 4, 5};
+            fhandler.LoadFromFile(data, y);
+            
+            SVMhandler <int>svmhandler;
+
+            svmhandler.fit(y, lbls, data);
+            std::cout<<"Accuracy>> "<<svmhandler.validate(data, y)<<std::endl;
+            break;
+        }
+        case 3: {
+            break;
+        }
+        default:
+
+            break;
+    }
     // Uncomment to generate centers
     //int res1 = getCentroids(centers, K_CLASSES);
     
@@ -20,28 +61,28 @@ int main(int argc, char** argv){
     //fhandler.Release();
 
     //Uncomment to Generate Training data"
-    
+    /*
     SingleFileHandler <float> fhandler("../models/centroids/centers_model.txt");
     fhandler.LoadFromFile(centers);
     int res2 = getHistograms(centers, K_CLASSES);
-    
+    */
    
     // Uncomment to train the data
-    /*
-    std::string trainingfile = "../models/training/trainingdata.txt";
-    std::string labelsfile   = "../models/training/labeldata.txt";
-    FileHandlerML <int> fhandler(trainingfile, labelsfile);
-
-    std::vector<std::vector<int>> data;
-    std::vector<int> y;
-    std::vector<int> lbls {0, 1, 2, 3, 4, 5};
-    fhandler.LoadFromFile(data, y);
     
-    SVMhandler <int>svmhandler;
+    //std::string trainingfile = "../models/training/trainingdata.txt";
+    //std::string labelsfile   = "../models/training/labeldata.txt";
+    //FileHandlerML <int> fhandler(trainingfile, labelsfile);
 
-    svmhandler.fit(y, lbls, data);
+    //std::vector<std::vector<int>> data;
+    //std::vector<int> y;
+    //std::vector<int> lbls {0, 1, 2, 3, 4, 5};
+    //fhandler.LoadFromFile(data, y);
+    //
+    //SVMhandler <int>svmhandler;
 
-    std::cout<<"Accuracy>> "<<svmhandler.validate(data, y)<<std::endl;
-    */
+    //svmhandler.fit(y, lbls, data);
+
+    //std::cout<<"Accuracy>> "<<svmhandler.validate(data, y)<<std::endl;
+    
     return 0;
 }
