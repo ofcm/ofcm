3164234940cd49c06d0a5bf999571c32f3bce99f
joseucsp <jose.chavez.alvarez@ucsp.edu.pe>
2019-02-20 22:34:34 -0500

    getting global centers for all videos

--- src/kmeans.cpp
+++ src/kmeans.cpp
@@ -11,7 +11,26 @@ void kmeans::setFeatures(std::vector<std::vector<float>> features){
     this->features = features;
 }
 
-void kmeans::runKmeans(std::vector<int>& result){
+std::vector<std::vector<float>> kmeans::getCentroids(){
+    return centers;
+}
+
+void kmeans::getHistogram(std::vector<int>& result)
+{
+    std::vector<std::vector<std::vector<float>>> cluster(numberClass);
+
+    for(auto feature : features)
+    {
+        int goodClass = getGoodCluster(feature);
+        cluster[goodClass].push_back(feature);
+    }
+
+    for(int k = 0;k < numberClass;k++){
+        result.push_back(cluster[k].size());
+    }
+}
+
+void kmeans::runKmeans(){
     std::vector<std::vector<std::vector<float>>> cluster(numberClass);
     oldCenters = centers;
 
@@ -21,7 +40,8 @@ void kmeans::runKmeans(std::vector<int>& result){
 
     while (flag == false){
         flag = true;
-        /*
+        //std::cout << "oldCenters size = " << oldCenters.size() << std::endl;
+        std::cout << "\ncentroids (it = "<< itr << ") :\n" << std::endl;
         for (int k = 0; k < numberClass; k++)
         {
             for (int icen = 0; icen < centers[k].size(); icen++)
@@ -30,15 +50,13 @@ void kmeans::runKmeans(std::vector<int>& result){
             }
             std::cout << std::endl;
         }
-        */
+        
         for (int ic = 0; ic < cluster.size(); ic++)
             cluster[ic].clear();
         for(auto feature : features){
             int goodClass = getGoodCluster(feature);
             cluster[goodClass].push_back(feature);
         }
-
-        //std::vector<std::vector<float>> newCenters(numberClass, std::vector<float>(12,0.0));
 
         for (int k = 0; k < numberClass; k++)
         {
@@ -51,7 +69,6 @@ void kmeans::runKmeans(std::vector<int>& result){
         for(int k = 0;k < numberClass;k++){
             for(int i = 0;i < cluster[k].size();i++){
                 for(int val = 0;val < cluster[k][i].size();val++){
-//                    std::cout << "k = "<< k << ", val = "<< val << " (i) = "<< i <<std::endl;
                     centers[k][val] += cluster[k][i][val]/cluster[k].size();
                 }
             }
@@ -59,34 +76,21 @@ void kmeans::runKmeans(std::vector<int>& result){
 
         
         for(int k = 0;k < numberClass;k++){
-
             float sum = 0.0;
-
-            //std::cout << "----->" << oldCenters[0].size() << std::endl;
-
             for(int i =0; i< centers[k].size();i++)
             {
                 sum += ((oldCenters[k][i] - centers[k][i])*(oldCenters[k][i] - centers[k][i]));
             }
                 
             sum = sqrt(sum);
-            //std::cout << "sum = " << sum << std::endl;
             if(sum <= limitError)
-            {
                 flag *= true;
-            }
-            else{
-                flag *= false;
-            }
-            
+            else
+                flag *= false;            
         }
         itr++;
         //std::cout << "itr = " << itr << std::endl;
         oldCenters = centers;
-    }
-    for(int k = 0;k < numberClass;k++){
-        //std::cout << "size = " << k << " -> "<< cluster[k].size() << std::endl;
-        result.push_back(cluster[k].size());
     }
 }
 
@@ -95,17 +99,6 @@ void kmeans::startingCenters(){
         int random = rand() % (features.size() - 1);
         centers.push_back(features[random]);
     }  
-    /*
-    for (int k = 0; k < numberClass; k++)
-    {
-        for (int icen = 0; icen < centers.size(); icen++)
-        {
-            std::cout << centers[k][icen] << " ";
-        }
-        std::cout << std::endl;
-    }
-    */
-
 }   
 
 int kmeans::getGoodCluster(std::vector<float> feature){
@@ -120,13 +113,11 @@ int kmeans::getGoodCluster(std::vector<float> feature){
             sum += (centers[k][i] - feature[i]) * (centers[k][i] - feature[i]);
         }
         sum = sqrt(sum);
-        //std::cout << "sum (" << k << ") = " << sum <<std::endl;
         if(sum < minError){
             minError = sum;
             gc = k;
         }
     }
-    //if (gc == 2)
-    //    std::cout << "***" << std::endl;
+
     return gc;
 }
