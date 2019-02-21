5bf0af081ac001311c0c4eb10ac7a9a3da77015a
erickTorneroT <erickdeivy01@gmail.com>
2019-02-21 02:51:13 -0500

    Parsing Centers ready

--- src/headers/miscellanius.hpp
+++ src/headers/miscellanius.hpp
@@ -5,6 +5,7 @@
 //# include <vector>
 # include "utils.hpp"
 # include <fstream>
+# include <typeinfo>
 
 template <class T>
 void printMatInConsole(const cv::Mat & matrix){
@@ -26,7 +27,7 @@ void print2DstdVector(const std::vector<std::vector<T>> matrix){
 }
 
 template <class T>
-class FileHandler{
+class FileHandlerML{
 
     private:
     std::fstream ftrain;
@@ -71,7 +72,7 @@ class FileHandler{
         return answer;
     }
     public:
-    FileHandler(std::string tr, std::string lbl):trainfile(tr), labelfile(lbl){};
+    FileHandlerML(std::string tr, std::string lbl):trainfile(tr), labelfile(lbl){};
 
     void AppendLine(std::vector<T> data, int label){
         if(data.size() == 0){
@@ -137,4 +138,100 @@ class FileHandler{
     }
 };
 
+template <class T>
+class SingleFileHandler{
+    private:
+    std::fstream fhanler;
+    std::string filename;
+    std::vector<T> ParseLine(std::string line){
+        std::vector<T> answer;
+        std::string numberst = "";
+        for(int i = 0; i < line.size(); i++){
+            if(line[i] != ','){
+                numberst.append(1, line[i]);
+            }
+            else{
+                try{
+                    T num;
+                    if(typeid(T) == typeid(int))
+                        num = std::stoi(numberst);
+                    else if (typeid(T) == typeid(float))
+                        num = std::stof(numberst);
+                    
+                    answer.push_back(num);
+                    numberst = "";
+                }
+                catch(const std::invalid_argument & ia){
+                    std::cout<<"Bad parsing!>> "<<numberst<<std::endl;
+                }
+                catch(const std::out_of_range & orng){
+                    std::cout<<"Out rangeZ! "<<numberst<<std::endl;
+                }
+            }
+        }
+        if(numberst.size() > 0){
+            try{
+                T num;
+                if(typeid(T) == typeid(int))
+                    num = std::stoi(numberst);
+                else if (typeid(T) == typeid(float))
+                    num = std::stof(numberst);
+                
+                answer.push_back(num);
+                numberst = "";
+            }
+            catch(const std::invalid_argument & ia){
+                std::cout<<"Bad parsing!>> "<<numberst<<std::endl;
+            }
+            catch(const std::out_of_range & orng){
+                std::cout<<"Out rangeZ! "<<numberst<<std::endl;
+            }
+        }
+        return answer;
+    }
+
+    public:
+    SingleFileHandler(std::string fln):filename(fln){};
+
+    bool LoadFromFile(std::vector<std::vector<T>> & data){
+        this->Release();
+        this->fhanler.open(this->filename, std::fstream::in);
+        std::string line;
+
+        while(std::getline(this->fhanler, line)){
+            std::vector<T> ans = this->ParseLine(line);
+            data.push_back(ans);
+            //std::cout<<ans.size()<<std::endl;
+        }
+        return true;
+    }
+    void Release(){
+        if(this->fhanler.is_open())
+            this->fhanler.close();
+    }
+    void AppendLine(std::vector<T> data){
+        if(data.size() == 0){
+            std::cout<<"Non data in vector!"<<std::endl;
+            return;
+        }
+        if(!this->fhanler.is_open())
+            this->fhanler.open(this->filename, std::ios_base::app);
+       
+        // Row data feature
+        int nFeatures = data.size();
+        
+        std::string line = "";
+        for(int j = 0; j < nFeatures; j++){
+            line.append(std::to_string(data[j]));
+            if(j!=nFeatures - 1)
+                line.append(",");
+            else
+                line.append("\n");
+        }
+
+        // Writing in files
+        fhanler.write(line.c_str(), line.size()*sizeof(char));
+    }
+    
+};
 # endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,16 @@
 #include "headers/utils.hpp"
 #include "headers/centroids.hpp"
+#include "headers/miscellanius.hpp"
 
 int main(int argc, char** argv){   
     std::vector<std::vector<float>> centers;
-    int K_CLASSES = 4*6;
+    int K_CLASSES = 2*6;
     int res = getCentroids(centers, K_CLASSES);
 
+    SingleFileHandler <float> fhandler(std::string("centers_model.txt"));
+    for(int i = 0; i < centers.size(); i++){
+        fhandler.AppendLine(centers[i]);
+    }
+    fhandler.Release();
     return 0;
 }
