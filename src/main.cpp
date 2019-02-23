455bbac35cfe725cc2e34834a260e907b7d66486
erickTorneroT <erickdeivy01@gmail.com>
2019-02-23 02:53:41 -0500

    Loading data training working

--- src/main.cpp
+++ src/main.cpp
@@ -68,15 +68,16 @@ int main(int argc, char** argv){
         case 2: {
             std::cout<<"\n==============================================\n";
             std::cout<<"Training the SVM classifier"<<std::endl;
-            std::vector<std::vector<int>> data;
-            std::vector<int> y;
+            std::vector<std::vector<float>> data;
+            std::vector<float> y;
             std::vector<int> lbls {0, 1, 2, 3, 4, 5};
-            //fhandler.LoadFromFile(data, y);
+            FileHandlerML <float, float> fhandler(TRAININGDATA_FILE, TRAININGLABEL_FILE);
+            fhandler.LoadFromFile(data, y);
 
-            SVMhandler <int>svmhandler;
+            //SVMhandler <float> svmhandler;
 
-            svmhandler.fit(y, lbls, data);
-            std::cout<<"Accuracy>> "<<svmhandler.validate(data, y)<<std::endl;
+            //svmhandler.fit(y, lbls, data);
+            //std::cout<<"Accuracy>> "<<svmhandler.validate(data, y)<<std::endl;
             //int val = svmhandler.SaveModel("../models/svm/svm_model");
             //if(val == 0)
             //    std::cout<<"Model SVM Saved"<<std::endl;
@@ -102,8 +103,8 @@ int main(int argc, char** argv){
             std::cout<<"Model loaded ... > "<<1<<std::endl;
             // Loading SVM model
             std::cout<<"Loading SVM model"<<std::endl;
-            SVMhandler <int> svmhandler;
-            svmhandler.LoadModelFromFile("../models/svm/svm_model");
+            //SVMhandler <int> svmhandler;
+            //svmhandler.LoadModelFromFile("../models/svm/svm_model");
 
             // Initialize K - means model
             //kmeans bwords(centers, K_CLASSES);
